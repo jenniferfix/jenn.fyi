1d43ff0287cd5d1e36d5afb96ba8fef056e57b16
Jennifer Fix <codingjennifer@gmail.com>
2026-05-28 23:12:46 -0600

    move firacode font to preloaded link

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -29,6 +29,16 @@ export const Route = createRootRoute({
 			},
 		],
 		links: [
+			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+			{
+				rel: "preconnect",
+				href: "https://fonts.gstatic.com",
+				crossOrigin: "anonymous",
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap",
+			},
 			{
 				rel: "stylesheet",
 				href: appCss,
